c18bbde20f6c6c4968afa8c173c75951745f16ee
Geojohn1983 <145591899+Geojohn1983@users.noreply.github.com>
2023-10-20 17:33:47 +0300

    Достаем трек-номер из json ответа сервера

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -6,7 +6,7 @@ import configuration
 # создаем новый заказ
 def post_new_order(order_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
-                         json=order_body)
+                         json=order_body).json()["track"]
 
 # получаем заказ по треку
 def get_order_info(track):
